11781f507a2e4b5711e97792844b39d24705c77f
InKasidis <inzeki@hotmail.com>
2018-05-05 10:49:15 +0700

    Initial commit

---
+++ app/src/main/java/kasidis/suthison/com/findfriend/MainActivity.java
@@ -0,0 +1,23 @@
+package kasidis.suthison.com.findfriend;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import kasidis.suthison.com.findfriend.fragment.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.contentMainFragment,new MainFragment())
+                    .commit();
+        }
+
+    }
+}
